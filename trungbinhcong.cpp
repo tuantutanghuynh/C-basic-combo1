2cf81db236262144f817e61edd7b78edd4829435
tuantutanghuynh <tanghuynhtuantuwork@gmail.com>
2025-08-13 11:30:33 +0700

    Create trungbinhcong.cpp

---
+++ trungbinhcong.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main() {
+        int a,b;
+        float tbCong;
+        printf("Nhap so nguyen a:");
+        scanf("%d",&a);
+        printf("Nhap so nguyen b:");
+        scanf("%d",&b);
+        tbCong=(float)(a+b)/2;
+        printf("Trung binh cong cua %d va %d la: %.2f",a,b,tbCong);
+      
+        
+
+    return 0; 
